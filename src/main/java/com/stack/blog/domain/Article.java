24c86b29d300728d6bbb55c69127c295c84b9761
shopmall99 <serbible2021@gmail.com>
2025-02-25 15:28:12 +0900

    07.Blog_View_Thymeleaf

--- src/main/java/com/stack/blog/domain/Article.java
+++ src/main/java/com/stack/blog/domain/Article.java
@@ -1,11 +1,54 @@
 package com.stack.blog.domain;
 
+// ~ch06까지
+//
+//import jakarta.persistence.*;
+//import lombok.AccessLevel;
+//import lombok.Builder;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//
+//@Entity
+//@Getter
+//@NoArgsConstructor(access = AccessLevel.PROTECTED)
+//public class Article {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Column(name = "id", updatable = false)
+//    private Long id;
+//
+//    @Column(name = "title", nullable = false)
+//    private String title;
+//
+//    @Column(name = "content", nullable = false)
+//    private String content;
+//
+//    @Builder
+//    public Article(String title, String content) {
+//        this.title = title;
+//        this.content = content;
+//    }
+//
+//    public void update(String title, String content) {
+//        this.title = title;
+//        this.content = content;
+//    }
+//}
+
+//ch07 View
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.LocalDateTime;
+
+@EntityListeners(AuditingEntityListener.class)
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -22,6 +65,15 @@ public class Article {
     @Column(name = "content", nullable = false)
     private String content;
 
+    @CreatedDate
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+
     @Builder
     public Article(String title, String content) {
         this.title = title;
@@ -33,3 +85,4 @@ public class Article {
         this.content = content;
     }
 }
+
